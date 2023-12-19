09a28545c33862cd9bd13361aeebfd881804bd28
Mabaclaan <122130349+Mabaclaan@users.noreply.github.com>
2023-12-19 12:14:46 -0800

    Sound effects complete

--- player.gd
+++ player.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
 var speed = 400
+var velocity
 
+@onready var bg_music = $"../../bg_music"
+@onready var enemy_hit = $"../../enemy_hit"
 
 func _ready():
 	pass # Replace with function body.
 
 
 func _process(delta):
+	
+	velocity = Vector2.ZERO
+	
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -1
 	if Input.is_action_pressed("move_right"):
@@ -18,8 +24,9 @@ func _process(delta):
 		velocity.y = 1
 	
 	velocity.normalized()
-	velocity = Vector2.ZERO
-
 
 func _on_body_entered(body):
 	hide()
+	bg_music.stop()
+	enemy_hit.play()
+	
