35f5c412f14b60ba7483dc0d7aef8194307b4250
Mabaclaan <122130349+Mabaclaan@users.noreply.github.com>
2023-12-19 11:25:51 -0800

    Player scene setup

---
+++ player.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var speed = 400
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -1
+	if Input.is_action_pressed("move_right"):
+		velocity.x = 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -1
+	if Input.is_action_pressed("move_down"):
+		velocity.y = 1
+	
+	velocity.normalized()
+	velocity = Vector2.ZERO
